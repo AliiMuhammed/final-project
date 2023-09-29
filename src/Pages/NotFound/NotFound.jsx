aec45bc0b1a2314e44c5fda5b84eb76dbe34e757
AliiMuhammed <mohamedtaha8000@gmail.com>
2023-09-29 06:48:19 +0300

    add not found page

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import notfoundImg from "../../Assets/images/not found/notFound.png";
+import "./style/notfound.css";
 function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <>
+      <section className="notFound-section">
+        <div className="container">
+          <div className="left">
+            <img src={notfoundImg} alt="notFoundImage" />
+          </div>
+          <div className="right">
+            <h1>
+              oops! <span> page not found.</span>
+            </h1>
+            <p>
+              Check that you typed the address correctly.go back to your
+              previous page or try using our site search to find something
+              specific.
+            </p>
+            <Link to={"/"} className="main-btn back-btn">
+              Back to home
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default NotFound
+export default NotFound;
---
+++ src/Pages/SignUp/SignUp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./style/signup.css";
+function SignUp() {
+  return <div>SignUp</div>;
+}
+
+export default SignUp;
